d3fcd52ed36c20cbd93beacd112051f5b3b764c6
Shiori Yamaguchi <siorin2006@gmail.com>
2025-11-07 12:23:05 +0900

    データ入力の変更

--- app5.js
+++ app5.js
@@ -4,6 +4,31 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use("/public", express.static(__dirname + "/public"));
 
+let station = [
+  { id:1, code:"JE01", name:"東京駅"},
+  { id:2, code:"JE07", name:"舞浜駅"},
+  { id:3, code:"JE12", name:"新習志野駅"},
+  { id:4, code:"JE13", name:"幕張豊砂駅"},
+  { id:5, code:"JE14", name:"海浜幕張駅"},
+  { id:6, code:"JE05", name:"新浦安駅"},
+];
+
+app.get("/keiyo", (req, res) => {
+  // 本来ならここにDBとのやり取りが入る
+  res.render('db2', { data: station });
+});
+
+app.get("/keiyo_add", (req, res) => {
+  let id = req.query.id;
+  let code = req.query.code;
+  let name = req.query.name;
+  let newdata = { id: id, code: code, name: name };
+  station.push( newdata );
+  res.redirect('/public/keiyo_add.html');
+
+});
+
+
 app.get("/hello1", (req, res) => {
   const message1 = "Hello world";
   const message2 = "Bon jour";
@@ -36,31 +61,57 @@ app.get("/omikuji2", (req, res) => {
   res.render( 'omikuji2', {result:luck} );
 });
 
-app.get("/janken", (req, res) => {
-  let hand = req.query.hand;
-  let win = Number( req.query.win );
-  let total = Number( req.query.total );
+app.get("/janken0", (req, res) => {
+  let win = Number( req.query.win ) || 0;
+  let total = Number( req.query.total ) || 0;
+  const value =req.query.jan;
+  let hand ='';
+    if(value == 1){
+      hand = 'グー';
+    } else if(value ==2){
+     hand = 'チョキ';
+    }else if(value ==3){
+     hand = 'パー';
+    }
   console.log( {hand, win, total});
   const num = Math.floor( Math.random() * 3 + 1 );
   let cpu = '';
   let judgement = '';
   if( num==1 ) cpu = 'グー';
   else if( num==2 ) cpu = 'チョキ';
-  else cpu = 'パー';
+  else if( num==3) cpu = 'パー';
+
+ 
+
   // ここに勝敗の判定を入れる
   // 以下の数行は人間の勝ちの場合の処理なので，
   // 判定に沿ってあいこと負けの処理を追加する
-  judgement = '勝ち';
-  win += 1;
-  total += 1;
-  const display = {
-    your: hand,
-    cpu: cpu,
-    judgement: judgement,
-    win: win,
-    total: total
+  
+ if(num==value){
+    judgement ='あいこ';
+    total += 1;
+  
+
   }
-  res.render( 'janken', display );
+  else if(num==1 && value==3||num==2 && value==1||num==3 && value==2){
+    judgement = '勝ち';
+    win += 1;
+    total += 1;
+  }
+  else {
+    judgement ='負け';
+    total += 1;
+  }  
+
+  const display = {
+      your: hand,
+      cpu: cpu,
+      judgement: judgement,
+      win: win,
+      total: total
+    }
+
+  res.render( 'janken0', display );
 });
 
 app.listen(8080, () => console.log("Example app listening on port 8080!"));
